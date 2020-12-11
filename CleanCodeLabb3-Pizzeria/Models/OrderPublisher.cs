4bf11909e6e560490fffc4051535d7436e8aa7eb
Emil <penzon88@gmail.com>
2020-12-11 15:33:04 +0100

    Added classes for boserver pattern

---
+++ CleanCodeLabb3-Pizzeria/Models/OrderPublisher.cs
@@ -0,0 +1,29 @@
+﻿namespace DefaultNamespace
+{
+    public class OrderPublisher
+    {
+        private string message;
+        private List<string> Order;
+
+        public void PostNewMessage(string message)
+        {
+            this.message = message;
+            this.
+        }
+
+        public void MarkMessageAsRead()
+        {
+            message = null;
+        }
+        
+        public bool hasNewMessage()
+        {
+            return message != null;
+        }
+
+        public string pollMessage()
+        {
+            return message;
+        }
+    }
+}
---
+++ CleanCodeLabb3-Pizzeria/Models/OrderSubscriber.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace
+{
+    public class OrderSubscriber
+    {
+        {
+            public void pollAndPrintMessage(Publisher publisher)
+            {
+                if (publisher.hasNewMessage())
+                {
+                    Console.WriteLine(publisher.pollMessage());
+                }
+                else
+                {
+                    Console.WriteLine("No new messages");
+                }
+            }
+        }
+    }
+}
