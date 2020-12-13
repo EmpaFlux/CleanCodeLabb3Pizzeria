a60515603bcbcdef0f6d8c47bd61e882fbc919e4
Luna <Hanken95@gmail.com>
2020-12-13 13:07:52 +0100

    removed unised testmethod

--- CleanCodeLabb3-Pizzeria Tests/DisplayTextTests.cs
+++ CleanCodeLabb3-Pizzeria Tests/DisplayTextTests.cs
@@ -8,23 +8,6 @@ namespace CleanCodeLabb3_Pizza_Tests
     [TestClass]
     public class DisplayTextTests
     {
-        [DataTestMethod]
-        public void PlaceOrder_ListOfOrderables_ReturnsListOfStringsWithAllToppingsProductsAndPrice()
-        {
-            //Arrange
-            List<Product> order = TestData.GetOrder();
-            var expected = new List<List<string>>() {
-                new List<string>() { "Margerita", "Cola" },
-                new List<string>() { "tomato sauce", "cheese", "pineapple" },
-                new List<string>() { "115.00" }
-            };
-            var program = new Program();
-
-            //Act
-            var actual = program.PlaceOrder(order);
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
+        
     }
 }
